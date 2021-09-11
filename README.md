2ba7707e0519c0b142f7c06847a64c9519c9e29d
totoro <1729code@gmail.com>
2021-09-11 22:05:45 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dotfile
+
+These are dotfiles I use as a backup incase my pc broke, or something disastrous happen.
+
+Check this out how I backup my dotfiles - [script](./update.sh).
---
+++ update.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+CUR_DIR=/media/antihero/Data/Programming/personal_blog/dotfiles
+
+i3() {
+    rsync -a \
+        ~/.config/i3 \
+        "$CUR_DIR/.config"
+}
+
+neovim() {
+    rsync -a \
+        ~/.config/nvim \
+        "$CUR_DIR/.config"
+}
+
+vim() {
+    rsync -a \
+        --exclude=plugged \
+        --exclude=doc \
+        --exclude=*.swp \
+        --exclude=.netrwhist \
+        ~/.vim \
+        "$CUR_DIR"
+}
+
+zathura() {
+    mkdir -p "$CUR_DIR/.config/zathura"
+    rsync -a \
+        ~/.config/zathura \
+        "$CUR_DIR/.config/zathura"
+}
+
+code() {
+    if [ ! -d "$CUR_DIR/.config/Code" ]; then
+        mkdir "$CUR_DIR/.config/Code"
+    fi
+    rsync -a \
+        --exclude=globalStorage \
+        --exclude=snippets \
+        --exclude=workspaceStorage \
+        ~/.config/Code/User \
+        "$CUR_DIR/.config/Code"
+}
+
+polybar(){
+    rsync -a \
+        ~/.config/polybar \
+        "$CUR_DIR/.config"
+}
+
+rofi(){
+    rsync -a \
+        --exclude=rofi-bookmarks \
+        --exclude=rofimoji \
+        --exclude=rofi-themes/.git \
+        ~/.config/rofi \
+        "$CUR_DIR/.config"
+}
+
+picom(){
+    rsync -a \
+        ~/.config/picom \
+        "$CUR_DIR/.config"
+}
+
+alacritty(){
+    rsync -a \
+        ~/.config/alacritty \
+        "$CUR_DIR/.config"
+}
+
+doom_emacs(){
+    rsync -a \
+        ~/.doom.d \
+        "$CUR_DIR"
+}
+
+# apps for which config is to be updated
+declare -a apps=(
+    "alacritty"
+    "picom"
+    "polybar"
+    "rofi"
+    "code"
+    "i3"
+    "neovim"
+    "vim"
+    "zathura"
+    "doom_emacs"
+)
+
+if [ ! -d "$CUR_DIR/.config" ]; then
+    mkdir "$CUR_DIR/.config"
+fi
+
+for app in "${apps[@]}"; do
+    echo "Updating $app"
+    # to update each app use the function named same as the app
+    $app
+done
